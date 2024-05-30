11c84c01a342d4a92bb46d7deac51691b2d4f1c7
Adam 76864 <gax35606@student.gdynia.merito.pl>
2024-05-30 16:34:00 +0200

    Dodaj pliki projektów.

---
+++ binding do klas C#/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+
+public class MediaItem : INotifyPropertyChanged
+{
+    private string title;
+    private string directorOrAuthor;
+    private string publisherOrStudio;
+    private string medium;
+    private TimeSpan length;
+    private DateTime releaseDate;
+
+    public string Title
+    {
+        get { return title; }
+        set
+        {
+            title = value;
+            OnPropertyChanged("Title");
+        }
+    }
+
+    public string DirectorOrAuthor
+    {
+        get { return directorOrAuthor; }
+        set
+        {
+            directorOrAuthor = value;
+            OnPropertyChanged("DirectorOrAuthor");
+        }
+    }
+
+    public string PublisherOrStudio
+    {
+        get { return publisherOrStudio; }
+        set
+        {
+            publisherOrStudio = value;
+            OnPropertyChanged("PublisherOrStudio");
+        }
+    }
+
+    public string Medium
+    {
+        get { return medium; }
+        set
+        {
+            medium = value;
+            OnPropertyChanged("Medium");
+        }
+    }
+
+    public string Length
+    {
+        get { return string.Format("{0}:{1}:{2}", length.Hours, length.Minutes, length.Seconds); }
+        set
+        {
+            length = TimeSpan.Parse(value);
+            OnPropertyChanged("Length");
+        }
+    }
+
+    public DateTime ReleaseDate
+    {
+        get { return releaseDate; }
+        set
+        {
+            releaseDate = value;
+            OnPropertyChanged("ReleaseDate");
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
